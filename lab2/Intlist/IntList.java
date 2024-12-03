579325f34a46f67845884804345f6dd8d7ce2d52
Yechen <13924957176@163.com>
2024-12-04 00:20:03 +0800

    lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -1,3 +1,5 @@
+import org.junit.Test;
+
 import java.util.Formatter;
 
 /**
@@ -82,7 +84,16 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null){
+            A = B;
+            return A;
+        }else if(A.rest == null){
+            A.rest = B;
+            return A;
+        }else{
+            A.rest = dcatenate(A.rest, B);
+            return A;
+        }
     }
 
     /**
@@ -91,7 +102,13 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) {
+            return B;
+        }else if(A.rest == null){
+            return new IntList(A.first, B);
+        }else{
+            return new IntList(A.first, catenate(A.rest, B));
+        }
     }
 
 
