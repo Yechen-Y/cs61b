c10c280921ac7ae587026d86f4cabaa94a5f9fcc
Yechen <13924957176@163.com>
2024-12-04 18:06:58 +0800

    fix pro0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,15 +1,26 @@
 public class NBody {
-
+    /**
+     * 从相关文件中获取宇宙的半径，使用In实例的相关methods
+     * @param FileName 文件名
+     * @param in 为In的实例
+     * @return Radius 宇宙的半径
+     */
     public static double readRadius(String FileName){
         double Radius;
         In in = new In(FileName);
-
+        /*从输入流中读取int、double、String*/
         in.readInt();
         Radius = in.readDouble();
 
         return Radius;
     }
 
+    /**
+     * 获取文件中所有的星球
+     * @param FileName 文件的名字
+     * @param N 星球的个数
+     * @return all_pla 星球的数组
+     */
     public static Planet[] readPlanets(String FileName){
         In in = new In(FileName);
         int N = in.readInt();
@@ -23,6 +34,14 @@ public class NBody {
         return all_pla;
     }
 
+    /**
+     *
+     * @param T 星球运动的总时间
+     * @param dt 星球运动的时间间隔
+     * @param radius 宇宙的半径
+     * @param xForces 各星球所受合外力的数组
+     * @return void
+     */
     public static void main(String[] args){
         double T = Double.parseDouble(args[0]);
         double dt = Double.parseDouble(args[1]);
@@ -39,13 +58,18 @@ public class NBody {
 
         for (double t = 0; t <= T; t += dt) {
 
+            /*更新各星球在x、y轴上的万有引力*/
             for (int i = 0; i < all_pla.length; i += 1){
                 xForces[i] = all_pla[i].calcNetForceExertedByX(all_pla);
                 yForces[i] = all_pla[i].calcNetForceExertedByY(all_pla);
             }
+
+            /*更新各星球位置*/
             for (int j = 0; j < all_pla.length; j += 1){
                 all_pla[j].update(dt, xForces[j], yForces[j]);
             }
+
+            /*画图*/
             StdDraw.picture(0, 0, "images/starfield.jpg");
             for (int index = 0; index < all_pla.length; index += 1) {
                 all_pla[index].draw();
@@ -53,6 +77,8 @@ public class NBody {
 
             StdDraw.show();
         }
+
+        /*打印各星球最后的信息*/
         StdOut.printf("%d\n", all_pla.length);
         StdOut.printf("%.2e\n", radius);
         for (int i = 0; i < all_pla.length; i++) {
