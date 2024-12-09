e7ba4098c4e0b81c7104e1910d61228cb4c87606
Yechen <13924957176@163.com>
2024-12-09 19:25:00 +0800

    proj1a

--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -61,7 +61,6 @@ public class LinkedListDequeTest {
 
 	/** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
 	public static void addRemoveTest() {
-
 		System.out.println("Running add/remove test.");
 
 		LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
