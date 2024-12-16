f6e51155b73e228b185866903bda315efcb51bf8
Yechen <13924957176@163.com>
2024-12-16 20:09:46 +0800

    fix hw 1

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -71,6 +71,9 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     @Override
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
+        if (isEmpty()) {
+            throw new RuntimeException("");
+        }
         return rb[first];
     }
 
